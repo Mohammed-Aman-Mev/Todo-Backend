b7878b26ba24b907c7111faab6f37d71d44a72c2
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2024-02-24 22:36:02 +0530

    server is crated

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("object")
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.get("/", (res, req) => {
+  res.json({
+    message: "hellow from backend",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is runing at port:${port}`);
+});
